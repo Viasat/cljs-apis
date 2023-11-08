fa1a1aacadb5e919a7ecce21a74ed149b67cec1d
Joel Martin <joel.martin@viasat.com>
2023-11-08 20:16:39 +0000

    [viasat.apis.github] add helpful warning messages.
    If the endpoint or action arg doesn't refer to an actual endpoint or
    action in the Octokit API then return an error and list the options.
    
    Add a check that all parameters needed by the resolved URL are present
    in the user's command line arguments. If not, fatal with an error
    listing the missing args.

--- src/viasat/apis/github.cljs
+++ src/viasat/apis/github.cljs
@@ -1,7 +1,9 @@
 (ns viasat.apis.github
   (:require [clojure.string :as S]
             [cljs-bean.core :refer [->clj]]
+            [clojure.set :refer [difference]]
             [promesa.core :as P]
+            [viasat.util :refer [fatal]]
             ["@octokit/rest" :refer [Octokit]]))
 
 
@@ -14,6 +16,26 @@
 
 (defn REST [client endpoint action opts]
   (P/let
-    [req (-> client (aget "rest") (aget endpoint) (aget action))
-     res (req (clj->js opts))]
+    [log (or (-> client (aget "log") (aget "log"))
+             list) ;; if no logger then use list to ignore
+     rest-obj (aget client "rest")
+     endpoint-obj (aget rest-obj endpoint)
+     _ (when (not endpoint-obj)
+         (fatal 1 (str "No endpoint '" endpoint "' found in: "
+                       (S/join ", " (sort (keys (js->clj rest-obj)))))))
+     action-obj (aget endpoint-obj action)
+     _ (when (not action-obj)
+         (fatal 1 (str "No action '" action "' found in: "
+                       (S/join ", " (sort (keys (js->clj endpoint-obj)))))))
+
+     url (-> action-obj (aget "endpoint") (aget "DEFAULTS") (aget "url"))
+     req-params (set (map second (re-seq #"{([^}]*)}" url)))
+     missing-params (difference req-params (set (map name (keys opts))))
+     _ (when (seq missing-params)
+         (fatal 1 (str "Missing param(s) from URL '" url "': "
+                       (S/join ", " missing-params))))
+
+     _ (log (str "URL: " url ", opts: " opts))
+
+     res (action-obj (clj->js opts))]
   (->clj res)))
